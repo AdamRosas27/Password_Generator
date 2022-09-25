318f17ac8e9a5ae035961cc365d1bcae3c36f729
Adam Rosas <adamrosas27@gmail.com>
2022-09-25 16:17:32 -0700

    Add try clause and if statement to determine if the length of the password is valid

--- generator.py
+++ generator.py
@@ -19,3 +19,5 @@ len_of_password = int(input("Enter the length of your password"))
 
 
 # Set the lower and upper bounds for the password. Length of password cannot be < 6 or > 50
+try:
+    if len_of_password < 6 or len_of_password > 50:
