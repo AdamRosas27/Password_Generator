994c8a2dd6fbb57b4a02cbe99901f39c4e5a4ae4
Adam Rosas <adamrosas27@gmail.com>
2022-09-25 16:30:55 -0700

    Add while loop

--- generator.py
+++ generator.py
@@ -14,13 +14,12 @@ class InvalidInput(Error):
     pass
 
 
-# Take an input from the user that will determine the length of the password
-len_of_password = int(input("\nEnter the length of your password."))
-
-
 # Set the lower and upper bounds for the password. Length of password cannot be < 6 or > 50
+while True:
+    len_of_password = int(input("\nDesired Password Length:"))
 try:
     if len_of_password < 6 or len_of_password > 50:
         raise InvalidInput
 except InvalidInput:
+
     print("\nThe length of the password cannot be less than 6 characters of greater than 50 characters.")
