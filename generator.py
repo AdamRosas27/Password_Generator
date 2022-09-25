41f6bcab5357f916e9d528a978004fd80fdbe861
Adam Rosas <adamrosas27@gmail.com>
2022-09-25 16:54:28 -0700

    Add while loop and password_len variable that takes in user input

--- generator.py
+++ generator.py
@@ -2,24 +2,7 @@
 # Import random module
 # Import String module
 import random
+import string
 
-# Define custom exception
-
-
-class Error(Exception):
-    pass
-
-
-class InvalidInput(Error):
-    pass
-
-
-# Set the lower and upper bounds for the password. Length of password cannot be < 6 or > 50
 while True:
-    len_of_password = int(input("\nDesired Password Length:"))
-try:
-    if len_of_password < 6 or len_of_password > 50:
-        raise InvalidInput
-except InvalidInput:
-
-    print("\nThe length of the password cannot be less than 6 characters of greater than 50 characters.")
+    password_len = int(input("\nDesired Password Length: "))
