60c95c25fd36a2a542758b2324af7d560eb235b7
Adam Rosas <adamrosas27@gmail.com>
2022-09-25 16:11:41 -0700

    Add length of password variable that uses the user input to determine the length they want their password to be

--- generator.py
+++ generator.py
@@ -4,5 +4,6 @@ import random
 import string
 
 # Take an input from the user that will determine the length of the password
+len_of_password = int(input("Enter the length of your password"))
 
 # Set the lower and upper bounds for the password. Length of password cannot be < 6 or > 50
