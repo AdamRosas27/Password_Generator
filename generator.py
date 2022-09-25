1e658d32df435c5ec4b6f12866e265be1114035f
Adam Rosas <adamrosas27@gmail.com>
2022-09-25 16:13:28 -0700

    Add comment

--- generator.py
+++ generator.py
@@ -1,3 +1,5 @@
+# Define custom exceptio
+
 # Import random module
 import random
 # Import String module
@@ -5,5 +7,6 @@ import string
 
 # Take an input from the user that will determine the length of the password
 len_of_password = int(input("Enter the length of your password"))
+
 
 # Set the lower and upper bounds for the password. Length of password cannot be < 6 or > 50
