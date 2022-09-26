88f95a08c606104f1e403dd2f6c460fefb13fffa
Adam Rosas <adamrosas27@gmail.com>
2022-09-25 17:07:53 -0700

    Add variables for letters, numbers, and symbols

--- generator.py
+++ generator.py
@@ -1,8 +1,11 @@
 
 # Import random module
 # Import String module
+from lib2to3.pygram import Symbols
 import random
 import string
+
+import symbol
 
 while True:
     password_len = int(input("\nDesired Password Length: "))
@@ -12,3 +15,8 @@ while True:
         print("\nSorry, passwords cannot be more than 50 characters in length. Try again.")
     else:
         break
+
+lower_case = string.ascii_lowercase
+upper_case = string.ascii_uppercase
+numbers = string.digits
+special_chars = string.punctuation
