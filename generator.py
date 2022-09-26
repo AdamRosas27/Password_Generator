b041052c22dfc022ade5f44a5abd323ea7a8527e
Adam Rosas <adamrosas27@gmail.com>
2022-09-25 17:04:25 -0700

    Update if else statement

--- generator.py
+++ generator.py
@@ -7,7 +7,7 @@ import string
 while True:
     password_len = int(input("\nDesired Password Length: "))
     if password_len < 6:
-        print("\nSorry, passwords cannot be less than 6 characters in length. Try again. ")
+        print("\nSorry, passwords cannot be less than 6 characters in length. Try again.")
     elif password_len > 50:
         print("\nSorry, passwords cannot be more than 50 characters in length. Try again.")
     else:
